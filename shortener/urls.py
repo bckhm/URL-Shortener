7d3c9ec88a60e9223e72773174c1ee1aa0e72fd5
beckhamwee <beckhamwee13@gmail.com>
2022-03-20 01:23:36 +0800

    Add function that allows for foward slashes

--- shortener/urls.py
+++ shortener/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name='index'),
-    path("shorten/<str:inurl>", views.check_url, name="check_url"),
+    # <path:inurl> to match all possible strings, including forward /
+    path('shorten/<path:inurl>/', views.check_url, name="check_url"),
     path("<str:shortened_url>", views.redirect_to_og, name="redirect_to_og")
 ]
